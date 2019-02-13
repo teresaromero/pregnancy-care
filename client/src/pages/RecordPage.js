0ee2c4b049524c2a3b256fda5e699884d1c91037
Teresa Romero <tromerolozano@gmail.com>
2019-02-13 16:59:09 +0100

    added timeline for appointments

--- client/src/pages/RecordPage.js
+++ client/src/pages/RecordPage.js
@@ -15,6 +15,8 @@ import { RecordBackgroundForm } from "../components/RecordBackgroundForm";
 import { PregnancyForm } from "../components/RecordPregnancyForm";
 import { InformationForm } from "../components/InformationForm";
 
+import "bulma-timeline";
+
 class _RecordPage extends React.Component {
   componentDidMount() {
     let { dispatch } = this.props;
@@ -92,8 +94,46 @@ class _RecordPage extends React.Component {
                 </div>
               </div>
             </div>
-
             <RecordPregnancy />
+
+            <div className="section">
+              <div class="timeline">
+                <header class="timeline-header">
+                  <span class="tag is-medium is-primary">Start</span>
+                </header>
+                <div class="timeline-item">
+                  <div class="timeline-marker" />
+                  <div class="timeline-content">
+                    <p class="heading">January 2016</p>
+                    <p>Timeline content - Can include any HTML element</p>
+                  </div>
+                </div>
+                <div class="timeline-item">
+                  <div class="timeline-marker is-image is-32x32">
+                    <img src="http://bulma.io/images/placeholders/32x32.png" />
+                  </div>
+                  <div class="timeline-content">
+                    <p class="heading">February 2016</p>
+                    <p>Timeline content - Can include any HTML element</p>
+                  </div>
+                </div>
+                <header class="timeline-header">
+                  <span class="tag is-primary">2017</span>
+                </header>
+                <div class="timeline-item">
+                  <div class="timeline-marker is-icon">
+                    <i class="fa fa-flag" />
+                  </div>
+                  <div class="timeline-content">
+                    <p class="heading">March 2017</p>
+                    <p>Timeline content - Can include any HTML element</p>
+                  </div>
+                </div>
+                <div class="timeline-header">
+                  <span class="tag is-medium is-primary">End</span>
+                </div>
+              </div>
+            </div>
           </React.Fragment>
         ) : (
           <Loader />
