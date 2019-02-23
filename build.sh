abe7d0cf44f1b3acffae3024e22548b5e3909f8c
Teresa Romero <tromerolozano@gmail.com>
2019-02-23 14:15:54 +0100

    minor changes

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 rm -rf ./client/build
 rm -rf ./server/public/
 cd client
-yarn install
-yarn build
+npm install
+npm audit fix
+npm build
 cd ..
 cp -r ./client/build/ ./server/public
 echo "*" > ./server/public/.gitignore
