4f1318a256ff05346c6d7cf1e0feee9afda2dbd8
Teresa Romero <tromerolozano@gmail.com>
2019-02-23 17:40:39 +0100

    fawesome root

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import '@fortawesome/fontawesome-free/css/all.min.css'
 import "./lib/myBulma.css";
 import {App} from "./App";
 import * as serviceWorker from "./serviceWorker";
